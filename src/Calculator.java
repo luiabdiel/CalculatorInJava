1cdf13009a10cac581f19e03308d4ba5f6491a35
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-04-11 22:18:49 -0300

    feat: subtraction

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,16 +3,22 @@ public class Calculator {
         int firstNumber = Integer.parseInt(args[1]);
         int secondNumber = Integer.parseInt(args[2]);
 
-        System.out.println("(sum, sub, mult, div) first number second number");
-
         if (args[0].equals("sum")) {
             sum(firstNumber, secondNumber);
-        } else {
+        }
+        else if (args[0].equals("sub")) {
+            subtraction(firstNumber, secondNumber);
+        }
+        else {
             System.out.println("Ops.. preencha os dados corretamente");
         }
     }
 
     static void sum(int firstNumber, int secondNumber) {
         System.out.println(firstNumber + secondNumber);
+    }
+
+    static void subtraction(int firstNumber, int secondNumber) {
+        System.out.println(firstNumber - secondNumber);
     }
 }
