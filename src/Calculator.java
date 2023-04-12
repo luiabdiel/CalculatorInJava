71d24d258d75ed9d4061d45d31e06c76553949df
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-04-11 22:15:38 -0300

    feat: sum

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,18 @@
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+        int firstNumber = Integer.parseInt(args[1]);
+        int secondNumber = Integer.parseInt(args[2]);
+
+        System.out.println("(sum, sub, mult, div) first number second number");
+
+        if (args[0].equals("sum")) {
+            sum(firstNumber, secondNumber);
+        } else {
+            System.out.println("Ops.. preencha os dados corretamente");
+        }
+    }
+
+    static void sum(int firstNumber, int secondNumber) {
+        System.out.println(firstNumber + secondNumber);
     }
 }
